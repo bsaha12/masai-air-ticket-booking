f3ff894b51aab2f6f4b50f67420fe2b0922c5afe
Bhaskar Saha <bhaskar12jan678@gmail.com>
2024-03-11 16:06:37 +0530

    Authentication  Completed

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,5 +1,23 @@
-const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
-const auth = (req, res, next) => {};
+const auth = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, "masai");
+      if (decoded) {
+        // console.log(decoded);
+        req.userID = decoded.userID;
+        next();
+      } else {
+        res.status(200).json({ msg: "You are not authorized" });
+      }
+    } catch (error) {
+      res.status(500).json({ error });
+    }
+  } else {
+    res.status(400).json({ msg: "Please Login" });
+  }
+};
 
 module.exports = { auth };
